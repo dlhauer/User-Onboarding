825c3249034722290256253debf6485c9ef86fc8
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-14 15:03:04 -0800

    Added validation

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { withFormik, Form, Field, setNestedObjectValues } from 'formik';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function UserForm({ values }) {
+function UserForm({ values, errors, touched }) {
   
   return (  
-    <div className='user-form'>
-      <Form>
-        <Field type='text' name='username' placeholder='username'/>
-        <Field type='email' name='email' placeholder='email'/>
-        <Field type='text' name='password' placeholder='password'/>
+    <div >
+      <Form className='user-form'>
+
+        <Field className='input' type='text' name='username' placeholder='username'/>
+        {touched.username && errors.username && (<p>Whoa, whoa, whoa, you can't just go using my website without a username, pal!</p>)}
+
+        <Field className='input' type='email' name='email' placeholder='email'/>
+        {touched.email && errors.email && (<p>Nah, nah, you best get an email in there real quick, brah.</p>)}
+
+        <Field className='input' type='password' name='password' placeholder='password'/>
+        {touched.password && errors.password && (<p>Fine, don't enter a password. You'll never be able to access my awesome website ever again!</p>)}
+
         <label>
-          I haven't read the <a href='https://youtu.be/dQw4w9WgXcQ'>terms of service</a>, but I agree to them anyway.
-          <Field as='checkbox' name='terms' checked={values.terms}/>
+          <Field className='input' type='checkbox' name='terms' checked={values.terms}/>
+          I haven't read the <a href='https://youtu.be/dQw4w9WgXcQ'> terms of service</a>, but I agree to them anyway.
+          {touched.terms && errors.terms && (<p>Can't finish if you don't accept them there terms.</p>)}
         </label>
         <button>Submit</button>
       </Form>
@@ -27,7 +36,14 @@ const FormikUserForm = withFormik( {
       password: password || '',
       terms: terms || false,
     }
-  }
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required(),
+    email: Yup.string().required(),
+    password: Yup.string().required(),
+    terms: Yup.boolean(true).required(),
+  }),
+
 })(UserForm);
 
 export default FormikUserForm;
